99280138d723af4c7b8a905928c24b72f285b91e
Luke Thomas <116568876+code-nybbler@users.noreply.github.com>
2024-11-05 16:38:54 -0500

    Add files via upload

--- brackets.js
+++ brackets.js
@@ -137,7 +137,7 @@ async function submitPlayerCodeForm() {
             if (player !== undefined && player !== null) {
                 $('#player-code-dialog').removeClass('show');
                 showToast(`Welcome back ${player.Name}!`);
-                if (player.Type === 1 && player.Answer === 'null') {
+                if (player.Type === 1 && player.Answer === '') {
                     setTimeout(function() {
                         $('#prompt-dialog p').text(bracket.CurrentPrompt);
                         $('#prompt-dialog').addClass('show');
