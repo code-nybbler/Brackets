9c40409f1b5ee1decc1a18a815654da8cc8403e1
Luke Thomas <116568876+code-nybbler@users.noreply.github.com>
2024-11-19 16:31:24 -0500

    Add files via upload

--- brackets.js
+++ brackets.js
@@ -336,8 +336,8 @@ function showVotes(matchup) {
     for (let v = 0; v < matchup.Player2Votes; v++) {
         $(`.player[data-matchup="${matchup.MatchupID}"][data-player="${matchup.Player2ID}"]`).siblings('.votes').append('<span class="vote"></span>');
     }
-    if (matchup.WinnerID !== matchup.Player1ID) $(`.player[data-matchup="${matchup.MatchupID}"][data-player="${matchup.Player1ID}"]`).append(`<span class="loser-slash"></span>`);
-    else if (matchup.WinnerID !== matchup.Player2ID) $(`.player[data-matchup="${matchup.MatchupID}"][data-player="${matchup.Player2ID}"]`).append(`<span class="loser-slash"></span>`);
+    if (matchup.WinnerID !== matchup.Player1ID) $(`.player[data-matchup="${matchup.MatchupID}"][data-player="${matchup.Player1ID}"]`).addClass('loser').append(`<span class="loser-slash"></span>`);
+    else if (matchup.WinnerID !== matchup.Player2ID) $(`.player[data-matchup="${matchup.MatchupID}"][data-player="${matchup.Player2ID}"]`).addClass('loser').append(`<span class="loser-slash"></span>`);
 }
 
 function showToast(text) {
